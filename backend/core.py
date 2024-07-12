ac6d7fd6a98eeab33cd90353a6c70c0179f4c97d
Diraj Thajali <thajalidiraj@gmail.com>
2024-07-12 16:51:40 -0700

    add memory to our bot

--- backend/core.py
+++ backend/core.py
@@ -3,13 +3,16 @@ from langchain.chains.retrieval import create_retrieval_chain
 import os
 load_dotenv()
 
+from typing import Any, Dict, List
+
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_pinecone import PineconeVectorStore
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     '''
     Retrieval + Generation. 
     This function takes a query, embeds it, retrieves the most similar documents 
@@ -27,13 +30,25 @@ def run_llm(query: str):
     # This prompt is combined with the retrieved documents (context) 
     # to generate the answer only based on the context.
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    
+
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=doc_search.as_retriever(), combine_docs_chain=stuff_documents_chain
+    # along with regular retrieved documents (context) we also need to add context from our chat history
+    # thus we create chat_history aware retriever
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase") 
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=doc_search.as_retriever(), prompt=rephrase_prompt
     )
-    result = qa.invoke(input={"input": query})
+
+    # qa = create_retrieval_chain(
+    #     retriever=doc_search.as_retriever(), combine_docs_chain=stuff_documents_chain
+    # )
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+
+    # result = qa.invoke(input={"input": query})
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     new_result = {
         "query": result["input"],
         "result": result["answer"],
--- main.py
+++ main.py
@@ -11,11 +11,14 @@ prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
 
 # initialize session state to persist the chat history. 
 # Once the user exits the app however, the chat history will be lost.
-if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"] = []
-
-if "chat_answers_history" not in st.session_state:
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
     st.session_state["chat_answers_history"] = []
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_history"] = []
 
 
 def create_sources_string(source_urls: Set[str]) -> str:
@@ -32,7 +35,10 @@ def create_sources_string(source_urls: Set[str]) -> str:
 if prompt:
     with st.spinner("Generating response.."):
         # returns {query, result, source_documents}
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
+        # chat_history is a list of tuples: [("human", "prompt"), ("ai", "response")]
 
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
@@ -44,6 +50,9 @@ if prompt:
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["result"]))
+
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
